7c0e94eb0e3e460c1fe5bca445acfe2842419a5a
Jinu Yi <dionisos198@naver.com>
2024-05-15 06:34:34 +0900

    Refactor: 리다이렉트 경로 쿠키에서 꺼내기 (#169)

--- src/main/java/tavebalak/OTTify/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/tavebalak/OTTify/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,5 +1,12 @@
 package tavebalak.OTTify.oauth.handler;
 
+import static tavebalak.OTTify.oauth.cookie.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -7,16 +14,11 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 import tavebalak.OTTify.common.constant.Role;
 import tavebalak.OTTify.oauth.CustomOAuth2User;
+import tavebalak.OTTify.oauth.cookie.CookieUtils;
+import tavebalak.OTTify.oauth.cookie.HttpCookieOAuth2AuthorizationRequestRepository;
 import tavebalak.OTTify.oauth.jwt.JwtService;
 import tavebalak.OTTify.oauth.redis.RefreshTokenRepository;
-import tavebalak.OTTify.user.entity.User;
 import tavebalak.OTTify.user.repository.UserRepository;
-import tavebalak.OTTify.user.service.UserService;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @Component
@@ -26,15 +28,17 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     private final JwtService jwtService;
     private final RefreshTokenRepository tokenRepository;
     private final UserRepository userRepository;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
         log.info("OAuth2 Login 성공!");
         try {
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
             // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
-            if(oAuth2User.getRole() == Role.GUEST) {
+            if (oAuth2User.getRole() == Role.GUEST) {
                 String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
                 String refreshToken = jwtService.createRefreshToken();
                 Long userId = oAuth2User.getUserId();
@@ -42,10 +46,11 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
                 response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
                 response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
 
-                String url = generateUrl(accessToken, refreshToken, userId);
+                String url = generateUrl(accessToken, refreshToken, userId, request, response);
 //                response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
 
-                jwtService.sendAccessAndRefreshToken(response, oAuth2User.getEmail(), accessToken, refreshToken);
+                jwtService.sendAccessAndRefreshToken(response, oAuth2User.getEmail(), accessToken,
+                        refreshToken);
 
                 response.sendRedirect(url);
 
@@ -55,7 +60,7 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 //                findUser.authorizeUser();
             } else {
                 log.info("===user login===");
-                loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
+                loginSuccess(request, response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
             }
         } catch (Exception e) {
             throw e;
@@ -63,7 +68,9 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private void loginSuccess(HttpServletRequest request, HttpServletResponse response,
+            CustomOAuth2User oAuth2User)
+            throws IOException {
         String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
         String refreshToken = jwtService.createRefreshToken();
         Long userId = oAuth2User.getUserId();
@@ -71,19 +78,21 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
         response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
         response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
 
-        String url = generateUrl(accessToken, refreshToken, userId);
+        String url = generateUrl(accessToken, refreshToken, userId, request, response);
         log.info("===redirect===");
 
-        jwtService.sendAccessAndRefreshToken(response, oAuth2User.getEmail(), accessToken, refreshToken);
+        jwtService.sendAccessAndRefreshToken(response, oAuth2User.getEmail(), accessToken,
+                refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
 
         response.sendRedirect(url);
 
     }
 
-    private String generateUrl(final String accessToken, final String refreshToken, final Long userId) {
+    private String generateUrl(final String accessToken, final String refreshToken,
+            final Long userId, HttpServletRequest request, HttpServletResponse response) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder url = sb.append("http://52.79.200.90:3000/login/oauth2/code")
+        StringBuilder url = sb.append(generateBaseUrl(request, response) + "login/oauth2/code")
                 .append("?")
                 .append("accessToken=")
                 .append(accessToken)
@@ -95,4 +104,23 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
                 .append(userId);
         return url.toString();
     }
+
+    private String generateBaseUrl(HttpServletRequest request, HttpServletResponse response) {
+
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue).orElse("");
+        clearAuthenticationAttributes(request, response);
+
+        return targetUrl;
+
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request,
+            HttpServletResponse response) {
+
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request,
+                response);
+    }
+
+
 }
